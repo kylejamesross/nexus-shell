489d046ad6bf09bae01755d21b81403a1cba3944
Kyle Ross <github@kylejamesross.com>
2025-01-30 20:20:59 -0700

    Fix syntax error

--- flake.nix
+++ flake.nix
@@ -5,59 +5,52 @@
       url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     astal,
+    ags,
   }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      pname = "nexus";
+    packages.${system}.default = pkgs.stdenvNoCC.mkDerivation rec {
+      name = "nexus";
       src = ./.;
-      dontUnpack = true;
 
-      nativeBuildInputs = with pkgs; [
-        wrapGAppsHook
-        gobject-introspection
+      nativeBuildInputs = [
+        ags.packages.${system}.default
+        pkgs.wrapGAppsHook
+        pkgs.gobject-introspection
       ];
 
-      buildInputs =
-        (with astal.packages.${system}; [
-          astal3
-          io
-          apps
-          battery
-          bluetooth
-          hyprland
-          mpris
-          network
-          notifd
-          powerprofiles
-          tray
-          wireplumber
-        ])
-        ++ (with pkgs; [
-          gjs
-        ]);
-
-      preFixup = ''
-        gappsWrapperArgs+=(
-          --prefix PATH : ${with pkgs;
-          lib.makeBinPath [
-            dart-sass
-            fzf
-          ]}
-        )
-      '';
+      buildInputs = with astal.packages.${system}; [
+        astal3
+        io
+        apps
+        battery
+        bluetooth
+        hyprland
+        mpris
+        network
+        notifd
+        powerprofiles
+        tray
+        wireplumber
+      ];
 
       installPhase = ''
-        mkdir -p "$out"/bin
-        install "$src" "$out"/bin/nexus
-      '';
+        mkdir -p $out/bin
+        ags bundle app.ts $out/bin/${name}
+
+      
+'';
     };
   };
 }
